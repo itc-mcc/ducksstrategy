8220daed850cbb12e91cd7ac53db642c177c3ebf
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2020-09-19 13:31:09 -0600

    Agregando QuackBehavior

---
+++ src/main/java/ducks/quackbehavior/MuteQuack.java
@@ -0,0 +1,7 @@
+package ducks.quackbehavior;
+
+public class MuteQuack implements QuackBehavior {
+	public void quack() {
+		System.out.println("<< Silence >>");
+	}
+}
---
+++ src/main/java/ducks/quackbehavior/Quack.java
@@ -0,0 +1,7 @@
+package ducks.quackbehavior;
+
+public class Quack implements QuackBehavior {
+	public void quack() {
+		System.out.println("Quack");
+	}
+}
