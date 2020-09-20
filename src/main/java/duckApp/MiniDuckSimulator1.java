de647df42755fb91f3bfd64d54e905e9f1d5cccb
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2020-09-20 08:47:52 -0600

    Segundo simulador...

---
+++ src/main/java/duckApp/MiniDuckSimulator1.java
@@ -0,0 +1,20 @@
+package duckApp;
+import duckstype.*;
+import duck.*;
+import ducks.flybehavior.*;
+
+public class MiniDuckSimulator1 {
+ 
+	public static void main(String[] args) {
+ 
+		Duck mallard = new MallardDuck();
+		mallard.performQuack();
+		mallard.performFly();
+   
+		Duck model = new ModelDuck();
+		model.performFly();
+		model.setFlyBehavior(new FlyRocketPowered());
+		model.performFly();
+
+	}
+}
